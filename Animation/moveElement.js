1183138e650866e4441c618135324056c9f909c2
ZHT <ZHT@DESKTOP-05R7QAA>
2018-06-21 22:38:58 +0800

    动画改进

--- Animation/moveElement.js
+++ Animation/moveElement.js
@@ -1,31 +1,37 @@
-function moveElement(elementID,final_x,final_y,interval) {
+function moveElement(elementID, final_x, final_y, interval) {
     var elem = document.getElementById(elementID);
     // 元素中是否有 movement 属性
-    if(elem.movement){
+    if (elem.movement) {
         clearTimeout(elem.movement);
     }
     var xpos = parseInt(elem.style.left);
     var ypos = parseInt(elem.style.top);
-    if (xpos == final_x && ypos == final_y){
+    if (xpos == final_x && ypos == final_y) {
         return false;
     }
 
-    if(xpos < final_x){
-        xpos ++;
+    var dist = 0;
+    dist = (final_x - xpos) / 10;
+    if (dist < 0) {
+        dist = Math.floor(dist);
     }
-    if(xpos > final_x){
-        xpos --;
+    else {
+        dist = Math.ceil(dist);
     }
-    if(ypos < final_y){
-        ypos ++;
+    xpos += dist;
+
+    dist = (final_y - ypos) / 10;
+    if (dist < 0) {
+        dist = Math.floor(dist);
     }
-    if(ypos > final_y){
-        ypos --;
+    else {
+        dist = Math.ceil(dist);
     }
+    ypos += dist;
 
     elem.style.left = xpos + "px";
     elem.style.top = ypos + "px";
-    var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+    var repeat = "moveElement('" + elementID + "'," + final_x + "," + final_y + "," + interval + ")";
     // 设置元素的 movement 属性
     elem.movement = setTimeout(repeat, interval);
 }
