8951bf14f0ec26c5a673507f6e34766032a2691d
ZHT <ZHT@DESKTOP-05R7QAA>
2018-06-22 21:59:46 +0800

    canvas 创建

---
+++ html5/grayscale.js
@@ -0,0 +1,38 @@
+function convertToGS(img) {
+    // 存储原始的彩色版
+    img.color = img.src;
+
+    // 创建灰度版
+    img.grayscale = createGSCanvas(img);
+
+    img.onmouseover = function () {
+        this.src = this.color;
+    }
+    img.onmouseout = function(){
+        this.src = this.grayscale;
+    }
+}
+
+function createGSCanvas(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img,0,0);
+
+    var c = ctx.getImageData(0,0,img.width,img.height);
+    for (let i = 0; i < c.height; i++) {
+       for (let j = 0; j < c.width; j++) {
+           var x = (i*4)*c.width + (j*4);
+           var r = c.data[x];
+           var g = c.data[x+1];
+           var b = c.data[x+2];
+           c.data[x] = c.data[x+1] = c.data[x+2] = (r+g+b)/3;
+       }
+    }
+    ctx.putImageData(c,0,0,0,0,c.width,c.height);
+    return canvas.toDataURL();
+}
+
+addLoadEvent(convertToGS(document.getElementById("avatar")));
